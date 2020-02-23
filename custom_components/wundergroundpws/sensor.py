d9dda477cdb6e3b828e40baa309530061924e611
israelshok <israels@traxretail.com>
2020-02-23 17:56:00 +0200

    1

--- custom_components/wundergroundpws/sensor.py
+++ custom_components/wundergroundpws/sensor.py
@@ -41,10 +41,7 @@ DEFAULT_LANG = 'en-US'
 
 MIN_TIME_BETWEEN_UPDATES = timedelta(minutes=5)
 
-# try:
-#     conf_file = config.get_default_config_dir() + '/configuration.yaml'
-# except:
-conf_file = '~/HomeAssistant/homeassistant/configuration.yaml'
+conf_file = '/config/configuration.yaml'
 load_config = config.load_yaml_config_file(conf_file)
 
 try:
